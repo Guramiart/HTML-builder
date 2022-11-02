ef6a410e2132ad61e9b26cde80c8ef7cd28676d8
Artem Pasko <guramibox777@gmail.com>
2022-11-02 16:41:59 +0300

    refactor: replace log with stdout

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,10 +2,11 @@ const os = require('node:os');
 const fs = require('node:fs');
 const path = require('node:path');
 const readline = require('node:readline');
+const { stdout } = require('node:process');
 
 const filePath = path.join(__dirname, 'text.txt');
 const stream = fs.createWriteStream(filePath, {encoding: 'utf-8'})
-    .on('open', () => console.log('It’s nice to meet you! Enter your text please:'));
+    .on('open', () => stdout.write(`It’s nice to meet you! Enter your text please:${os.EOL}`));
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -17,4 +18,4 @@ rl.on("line", function(input) {
     else {stream.write(`${input}${os.EOL}`);}
 });
 
-rl.on("close", () => console.log('Have a good day!'));
+rl.on("close", () => stdout.write(`Have a good day!`));
