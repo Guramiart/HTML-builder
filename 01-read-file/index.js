ee98e0bebd2e169195de1d5320e24adece5913d1
Artem Pasko <guramibox777@gmail.com>
2022-11-02 10:59:52 +0300

    refactor: replace log with stdout

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,11 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { stdout } = require('node:process');
 
 const filePath = path.join(__dirname, 'text.txt');
 let stream = fs.createReadStream(filePath, {encoding: 'utf-8'});
 
 stream.on('readable', () => {
     let data = stream.read();
-    if(data) console.log(data); 
+    if(data) stdout.write(data); 
 });
