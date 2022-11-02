f91393bd8deac78af99f7789828571690eec683d
Artem Pasko <guramibox777@gmail.com>
2022-11-02 11:37:40 +0300

    refactor: replace with async function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,39 @@
+const os = require('node:os');
 const fs = require('node:fs');
 const path = require('node:path');
+const { stdout } = require('node:process');
 
 const srcPath = path.join(__dirname, 'files');
 const destPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(destPath, {recursive: true}, (err) => {
-    if(err) throw err;
-    console.log('Directory has been successfully created!');
-});
+copyFile();
 
-fs.readdir(destPath, {withFileTypes: true}, (err, files) => {
-    if(err) {throw err;}
-    for(let file of files) {
-        fs.unlink(path.join(destPath, file.name), (err) => {if(err) throw err});
-    }
-});
+function createDirectory() {
+    return new Promise(resolve => {
+        fs.mkdir(destPath, {recursive: true}, (err) => {
+            if(err) throw err;
+            resolve(`Directory has been successfully created!${os.EOL}`);
+        });
+    });
+}
 
-fs.readdir(srcPath, {withFileTypes: true}, (err, files) => {
-    if(err) {throw err;}
-    for(let file of files) {
-        if(file.isFile()) {
-            fs.copyFile(path.join(srcPath, file.name), path.join(destPath, file.name), (err) => {if(err) throw err});
+async function copyFile() {
+    let cd = await createDirectory();
+    stdout.write(cd);
+    fs.readdir(destPath, {withFileTypes: true}, (err, files) => {
+        if(err) {throw err;}
+        for(let file of files) {
+            fs.unlink(path.join(destPath, file.name), (err) => {if(err) throw err});
         }
-    }
-    console.log('Files has been successfully copied!');
-});
+
+        fs.readdir(srcPath, {withFileTypes: true}, (err, files) => {
+            if(err) {throw err;}
+            for(let file of files) {
+                if(file.isFile()) {
+                    fs.copyFile(path.join(srcPath, file.name), path.join(destPath, file.name), (err) => {if(err) throw err});
+                }
+            }
+            stdout.write(`Files has been successfully copied!${os.EOL}`);
+        });
+    });
+}
