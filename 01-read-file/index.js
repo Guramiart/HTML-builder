0008b25f30297e34ec686ace844fbfc44c4bba18
Artem Pasko <guramibox777@gmail.com>
2022-11-03 10:03:14 +0300

    refactor: constant naming

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,8 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { stdout } = require('node:process');
 
-const filePath = path.join(__dirname, 'text.txt');
-let stream = fs.createReadStream(filePath, {encoding: 'utf-8'});
+const FILE_PATH = path.join(__dirname, 'text.txt');
+let stream = fs.createReadStream(FILE_PATH, {encoding: 'utf-8'});
 
 stream.on('readable', () => {
     let data = stream.read();
