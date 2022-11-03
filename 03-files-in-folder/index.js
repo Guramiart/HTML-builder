de9f1195dbdc551fee53e173156d701531544a35
Artem Pasko <guramibox777@gmail.com>
2022-11-03 11:35:52 +0300

    refactor: constant naming

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,13 +3,13 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { stdout } = require('node:process');
 
-const filePath = path.join(__dirname, 'secret-folder');
+const FILE_PATH = path.join(__dirname, 'secret-folder');
 
-fs.readdir(filePath, {withFileTypes: true}, (err, files) => {
+fs.readdir(FILE_PATH, {withFileTypes: true}, (err, files) => {
     if(err) throw err;
     for(let file of files) {
         if(file.isFile()) {
-            fs.stat(path.join(filePath, file.name), (err, stats) => {
+            fs.stat(path.join(FILE_PATH, file.name), (err, stats) => {
                 if(err) throw err;
                 let fileName = path.basename(file.name).split('.').slice(0, -1).join('.');
                 let fileExt = path.extname(file.name).slice(1);
