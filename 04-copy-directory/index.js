d1eec1a79051ce86bc5a5fb16945fb892e9eb68a
Artem Pasko <guramibox777@gmail.com>
2022-11-04 16:20:32 +0300

    refactor: constant naming

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,14 +3,14 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { stdout } = require('node:process');
 
-const srcPath = path.join(__dirname, 'files');
-const destPath = path.join(__dirname, 'files-copy');
+const SRC_PATH = path.join(__dirname, 'files');
+const DEST_PATH = path.join(__dirname, 'files-copy');
 
 copyFile();
 
 function createDirectory() {
     return new Promise(resolve => {
-        fs.mkdir(destPath, {recursive: true}, (err) => {
+        fs.mkdir(DEST_PATH, {recursive: true}, (err) => {
             if(err) throw err;
             resolve(`Directory has been successfully created!${os.EOL}`);
         });
@@ -20,17 +20,17 @@ function createDirectory() {
 async function copyFile() {
     let cd = await createDirectory();
     stdout.write(cd);
-    fs.readdir(destPath, {withFileTypes: true}, (err, files) => {
+    fs.readdir(DEST_PATH, {withFileTypes: true}, (err, files) => {
         if(err) {throw err;}
         for(let file of files) {
-            fs.unlink(path.join(destPath, file.name), (err) => {if(err) throw err});
+            fs.unlink(path.join(DEST_PATH, file.name), (err) => {if(err) throw err});
         }
 
-        fs.readdir(srcPath, {withFileTypes: true}, (err, files) => {
+        fs.readdir(SRC_PATH, {withFileTypes: true}, (err, files) => {
             if(err) {throw err;}
             for(let file of files) {
                 if(file.isFile()) {
-                    fs.copyFile(path.join(srcPath, file.name), path.join(destPath, file.name), (err) => {if(err) throw err});
+                    fs.copyFile(path.join(SRC_PATH, file.name), path.join(DEST_PATH, file.name), (err) => {if(err) throw err});
                 }
             }
             stdout.write(`Files has been successfully copied!${os.EOL}`);
